ce6b457c96f44dea00bf52f11957b4f7613d5f6c
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2021-10-19 19:12:28 +0530

    Add files via upload

---
+++ Day 64/IntersectionofTwoArraysII.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        // result vector
+        vector<int> res;
+        // iterating on nums1 vector
+        for(int i=0; i<nums1.size(); i++){
+            // finding the same element
+            auto it = find(nums2.begin(), nums2.end(), nums1[i]);
+            // if we find it, pushing it into result vector
+            if(it!=nums2.end()){
+                res.push_back(nums1[i]);
+                nums2.erase(it);
+            }
+        }
+        return res;
+    }
+};
